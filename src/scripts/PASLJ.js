2cef1e7db29b5fd72a4437c1684b88c201e525b1
Rokas Valeckas <rvaleckas@uk.ey.com>
2017-09-21 16:33:52 +0100

    continues

--- src/scripts/PASLJ.js
+++ src/scripts/PASLJ.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //  https://msdn.microsoft.com/en-us/library/office/jj860569.aspx
 
     var FilteredContent = [];
-     var FilteredContent2 = [];
+    var FilteredContent2 = [];
 
     var Ajax = function (url) {
         return $.ajax({
@@ -89,15 +89,8 @@ $(document).ready(function () {
                     var string = arraystuff.toString();
                     $(this).html(string);
                 });
-                
-                $('.pinwrap').on('click', function (e) {
-                    $('.greywrap').not(this).removeClass('activedesc').find('.greydescription').slideUp();
-                    if ($(this).hasClass('activedesc')) {
-                        $(this).removeClass('activedesc').find('.greydescription').slideUp();
-                    } else {
-                        $(this).addClass('activedesc').find('.greydescription').slideDown();
-                    }
-                });
+
+
             };
 
             var FJS = FilterJS(FilteredContent, '#results', {
@@ -126,9 +119,10 @@ $(document).ready(function () {
 
 
 
- var Next2 = function (url) {
+    var Next2 = function (url) {
         $.when(Ajax(url)).then(function (data) {
-//            console.log(data)
+           
+            var count = 0;
             $.each(data.d.results, function (key, data) {
                 for (var key in data) {
                     if (data[key] === null && key !== 'Title' && key !== 'gpqg' && key !== 'Course_x0020_Code' && key !== 'Short_x0020_description' && key !== 'America_x0027_s_x0020_classroom_' && key !== 'America_x0027_s_x0020_AA_x0020_C' && key !== 'Blended_x0020_learning_x0020_pro') {
@@ -138,75 +132,67 @@ $(document).ready(function () {
                     }
                 }
 
-                $.each(data.Learning_x0020_Journey_x0020_nam.results, function (key, dat) {
+                $.each(data.Learning_x0020_Journey_x0020_nam.results, function (i, dat) {
+
                     if (dat === "Welcome to PAS") {
-                        FilteredContent2.push({
-                            Area: data.Area.results,
-                            SubServiceLine: data.Sector.results,
-                            ranks: data.Rank.results,
-                            Division: data.Division.results,
-                            RiskCapabilities: data.Risk_x0020_Capabilities_x0020__x.results,
-                            PiCapabilities: data.PI_x0020_Capabilities_x0020__x00.results,
-                            RiskServiceOfferings: data.Risk_x0020_Service_x0020_Offerin.results,
-                            PIServiceOfferings: data.PI_x0020_Service_x0020_Offerings.results,
-                            LearningJourneyname: data.Learning_x0020_Journey_x0020_nam.results,
-                            CourseLevel: data.Course_x0020_Level.results,
-                            Region: data.Region.results,
-                            Competency: data.Consultancy_x0020_Competency.results,
-                            Sector: data.Sector.results,
-                            PASPillars: data.PAS_x0020_Pillars.results,
-                            PASfferings: data.PAS_x0020__x002d__x0020_Offering.results,
-                            GrowthDrivers: data.Growth_x0020_Drivers.results,
-                            LearningType: data.Learning_x0020_Type0.results,
-                            CourseType: data.Learning_x0020_type.results,
-                            PASPillarOfferings: data.temp.results,
+                        count++;
+                        var CourseType;
+                        var Sector;
 
-                            URL: data.Course_x0020_URL.Url,
-
-                            Title: data.Title,
-                            Duration: data.gpqg,
-                            coursecode: data.Course_x0020_Code,
-                            description: data.Short_x0020_description,
-                            America: data.America_x0027_s_x0020_classroom_,
-                            AmericaAACPE: data.America_x0027_s_x0020_AA_x0020_C,
-                            Blended: data.Blended_x0020_learning_x0020_pro
+                        $.each(data.Learning_x0020_type.results, function (key, data) {
+                            CourseType = data;
                         });
+                        $.each(data.Sector.results, function (key, data) {
+                            Sector = data;
+                        });
+
+                        $('#resultss').append(
+                                '<div id="fjs_' + count + '" class="pointer">'
+                                + '<div class="number"></div>'
+                                + '<div class="smallcyrkle">'
+                                + '<div class="rotatedline"></div>'
+                                + '<div class="strightline"></div>'
+                                + '<div class="cyrcklecontent">'
+                                + '<div class="pinwrap clearfix">'
+                                + '<div class="colorbar"></div>'
+                                + '<div class="greywraptitle">' + data.Title + '</div>'
+                                + '<div class="greydescription">'
+                                + '<div class="pad2">'
+                                + '<div class="xx"></div>'
+                                + '<div class="xy"></div>'
+                                + '<div class="yx"></div>'
+                                + '<div class="yy"></div>'
+                                + data.Short_x0020_description
+                                + '<a class="learnmoregreen" target="_blank" href="' + data.Course_x0020_URL.Url + '">Launch Now</a>'
+                                + '</div>'
+                                + '</div>'
+                                + '<div class="learnmoreinbox">Learn More</div>'
+                                + '<div class="clearfix pad">Course type:<div class="padedwidth ">' + CourseType + '</div></div>'
+                                + '<div class="clearfix pad">Course code:<div class="padedwidth ">' + data.Course_x0020_Code + '</div></div>'
+                                + '<div class="clearfix pad">Sector: <div class="padedwidth clearfix">' + Sector + '</div></div>'
+                                + '<div class="clearfix pad">Course Duration:<div class="padedwidth clearfix courseduration">' + data.gpqg + '</div></div>'
+                                + '</div>'
+                                + '</div>'
+                                + '</div>'
+                                + '</div>'
+                                );
                     }
                 });
             });
 
-          
-
-            var bla = function (result, jQ) {
-//                $("#Divisiontrigger").trigger("click");
-
-                $('.greywrap').on('click', function (e) {
-                    $('.greywrap').not(this).removeClass('activedesc').find('.greydescription').slideUp();
-                    if ($(this).hasClass('activedesc')) {
-                        $(this).removeClass('activedesc').find('.greydescription').slideUp();
-                    } else {
-                        $(this).addClass('activedesc').find('.greydescription').slideDown();
-                    }
-                });
-
-                $('.courseduration').each(function () {
-                    var courseduration = $(this).html();
-                    var arraystuff = secondsToString(courseduration);
-                    var string = arraystuff.toString();
-                    $(this).html(string);
-                });
-            };
-
-            var FJS = FilterJS(FilteredContent2, '#resultss', {
-                template: '#list2',
-                callbacks: {
-                    afterAddRecords: bla
+            $('.pinwrap').on('click', function (e) {
+                $('.greywrap').not(this).removeClass('activedesc').find('.greydescription').slideUp();
+                if ($(this).hasClass('activedesc')) {
+                    $(this).removeClass('activedesc').find('.greydescription').slideUp();
+                } else {
+                    $(this).addClass('activedesc').find('.greydescription').slideDown();
                 }
             });
-            FJS.addCriteria({field: 'LearningJourneyname', ele: '#LearningJourneyname'});
-            
-            window.FJS = FJS;
+
         });
+
+
+
     };
     Next2(sourseUrl);
 
